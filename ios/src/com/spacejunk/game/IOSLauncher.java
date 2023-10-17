457c890cfe7d8d76561f3924d1232412be7f8966
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-10-17 11:06:54 -0400

    feat: Redo ios icons and remove launch screen

--- ios/src/com/spacejunk/game/IOSLauncher.java
+++ ios/src/com/spacejunk/game/IOSLauncher.java
@@ -3,20 +3,53 @@ package com.spacejunk.game;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.spacejunk.game.SpaceJunk;
+import com.spacejunk.game.interfaces.SystemServices;
 
-public class IOSLauncher extends IOSApplication.Delegate {
+public class IOSLauncher extends IOSApplication.Delegate implements SystemServices {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new SpaceJunk(), config);
+        return new IOSApplication(new SpaceJunk(SpaceJunk.DIFFICULTY_LEVEL.EASY, this), config);
     }
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
+    }
+
+    @Override
+    public void setSettings(boolean sound, boolean record, boolean vibrate) {
+        Preferences prefs = Gdx.app.getPreferences("SpaceJunkPreferences");
+
+        //Set values
+        prefs.putBoolean("sound", sound);
+        prefs.putBoolean("record", record);
+        prefs.putBoolean("vibrate", vibrate);
+
+        prefs.flush();
+    }
+
+    @Override
+    public boolean[] getSettings() {
+        Preferences prefs = Gdx.app.getPreferences("SpaceJunkPreferences");
+        return new boolean[] { prefs.getBoolean("sound", true),
+                prefs.getBoolean("record", true),
+                prefs.getBoolean("vibrate", true)};
+    }
+
+    @Override
+    public void startRecording(int xMax, int yMax, boolean recordAudioSetting) {
+
+    }
+
+    @Override
+    public void stopRecording() {
+
     }
 }
